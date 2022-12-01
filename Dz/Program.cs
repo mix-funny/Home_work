9ba7207064df33dee4f28ed4a622ce055361e8b4
Michael_SVAO <vierumihail7@gmail.com>
2022-12-01 19:10:25 +0300

    Задача №2

---
+++ Dz/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int a = 5;
+int b = 7;
+int max = a;
+
+if(b > max) max = b;
+Console.Write("Большее число из первой пары чисел => ");
+Console.WriteLine(max);
+
+int a1 = 2;
+int b1 = 10;
+int max1 = a1;
+
+if(b1 > max1) max1 = b1;
+Console.Write("Большее число из второй пары чисел => ");
+Console.WriteLine(max1);
+
+int a2 = -9;
+int b2 = -3;
+int max2 = a2;
+
+if(b2 > max2) max2 = b2;
+Console.Write("Большее число из третей пары чисел => ");
+Console.WriteLine(max2);
