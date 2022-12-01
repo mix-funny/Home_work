75d83624574b95eb4e3ba2656dd5ae3b07ad5f49
Michael_SVAO <vierumihail7@gmail.com>
2022-12-01 20:23:19 +0300

    задача №2

--- Dz/Program.cs
+++ Dz/Program.cs
@@ -2,23 +2,33 @@
 int a = 5;
 int b = 7;
 int max = a;
+int min = 0;
+int min1 = 0;
+int min2 = 0;
 
-if(b > max) max = b;
+
+if(b > max) max = b; min = a;
 Console.Write("Большее число из первой пары чисел => ");
 Console.WriteLine(max);
+Console.Write("Наименьшее число из первой пары чисел => ");
+Console.WriteLine(min);
 
 int a1 = 2;
 int b1 = 10;
 int max1 = a1;
 
-if(b1 > max1) max1 = b1;
+if(b1 > max1) max1 = b1; min1 = a1;
 Console.Write("Большее число из второй пары чисел => ");
 Console.WriteLine(max1);
+Console.Write("Наименьшее число из второй пары чисел => ");
+Console.WriteLine(min1);
 
 int a2 = -9;
 int b2 = -3;
 int max2 = a2;
 
-if(b2 > max2) max2 = b2;
+if(b2 > max2) max2 = b2; min2 = a2;
 Console.Write("Большее число из третей пары чисел => ");
 Console.WriteLine(max2);
+Console.Write("Наименьшее число из третей пары чисел => ");
+Console.WriteLine(min2);
