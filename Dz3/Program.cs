0f5bcd78d44814cb001647071e7f5b26314be85f
Michael_SVAO <vierumihail7@gmail.com>
2022-12-01 22:41:39 +0300

    Задача №6

---
+++ Dz3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число...");
+int A = int.Parse(Console.ReadLine()!);
+int result = A%2;
+
+if(result == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
