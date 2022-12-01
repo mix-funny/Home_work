60fbf997c8cb5082d6db8eb0773da36ea93679cb
Michael_SVAO <vierumihail7@gmail.com>
2022-12-01 20:36:55 +0300

    Задача №4

---
+++ Dz2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+int a = 2;
+int b = 3;
+int c = 7;
+int max = a;
+
+
+if(b > max) max = b;
+if(c > max) max = c;
+Console.Write("Большее число из первой тройки чисел => ");
+Console.WriteLine(max);
+
+int a1 = 44;
+int b1 = 5;
+int c1 = 78;
+int max1 = a1;
+
+if(b1 > max1) max1 = b1;
+if(c1 > max1) max1 = c1;
+Console.Write("Большее число из второй тройки чисел => ");
+Console.WriteLine(max1);
+
+int a2 = 22;
+int b2 = 3;
+int c2 = 9;
+int max2 = a2;
+
+if(b2 > max2) max2 = b2;
+if(c2 > max2) max2 = c2;
+Console.Write("Большее число из третей тройки чисел => ");
+Console.WriteLine(max2);
